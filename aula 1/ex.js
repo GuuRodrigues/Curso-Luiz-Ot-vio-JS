5c40235e8acb07b066d3e452c5c3b534c8016be5
Augusto Rodrigues <augustorodrigues14981@gmail.com>
2023-11-28 20:21:38 -0300

    iniciando curso de js da Udemi

---
+++ aula 1/ex.js
@@ -0,0 +1,10 @@
+const name = 'Augusto'
+const lastName = 'Rodrigues'
+let age = 20
+let weight = 62.00
+let height = 1.72
+
+let imc = weight / (height * height)
+let bornDate = 2023 - age;
+
+console.log(`${name} ${lastName} tem ${age} anos, nascido em ${bornDate}, pesa ${weight}kg tem ${height}m de altura e seu IMC é ${imc}`)
---
+++ aula 1/index.js
@@ -0,0 +1,3 @@
+let nome = 'Augusto'
+
+console.log(`Meu nome é "${nome}". Estou aprendendo JavaScript ás`, 10, `da manhã. `)
